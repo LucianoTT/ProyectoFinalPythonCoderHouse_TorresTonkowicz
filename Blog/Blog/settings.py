abb58e564dcee7392b42dd1230f0233600f2114b
LucianoTT <ototinul24@gmail.com>
2023-10-21 13:02:44 -0300

    cambio de configuracion debug
    Cambie la configuracion del DEBUG de false a true porque no carga las imagenes aun con el ALLOWED_HOSTS con localhost

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a(rwxelk76yicth&4qxnlm00clp7-%g$$b&^)k!x20625b!z9o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
